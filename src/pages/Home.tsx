fbf6e8e43f16d6271a01132140f92cc5dd7ce93b
Antoine <adrouhin7@gmail.com>
2026-01-25 18:34:41 +0100

    Remove TattooMachine component - will implement 3D model differently

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 import SectionTitle from '@/components/SectionTitle';
 import CardTattooStyle from '@/components/CardTattooStyle';
 import GalleryGrid from '@/components/GalleryGrid';
-import TattooMachine from '@/components/TattooMachine';
+
 import { MapPin, Clock, HeartHandshake, ShieldCheck } from 'lucide-react';
 import ContactForm from '@/components/ContactForm';
 import LocationMap from '@/components/LocationMap';
@@ -180,7 +180,6 @@ const Home: React.FC = () => {
 
   return (
     <div className="bg-capone-black text-capone-white" style={{ position: "relative" }}>
-      <TattooMachine />
       {/* ACCUEIL SECTION */}
       <section id="accueil" className="min-h-screen">
         {/* Hero Section */}
